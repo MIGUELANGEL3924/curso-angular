dda704809c17ac90d4586e3477eed1ffa4d19e4f
MIGUELANGEL3924 <mkitito2484@gmail.com>
2024-03-17 17:14:13 -0500

    event binding keydown

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -34,6 +34,11 @@ export class LabsComponent {
   changeHandler(event: Event) {
     console.log(event);
   }
+  keydownHandler(event: KeyboardEvent) {
+    const input = event.target as HTMLInputElement;
+    console.log(input.value);
+
+  }
 }
 
 
